87befb73449cdd7f79c55131a6041d090a2bab8c
Grigorii Bolotov <Grigorybolotov@yandex.ru>
2022-03-23 22:05:16 +0300

    Initial Commit

---
+++ iOS5-HW10-BG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS5-HW10-BG
+//
+//  Created by Григорий Болотов  on 23.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
